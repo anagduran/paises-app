66612353675910fd21d6c8f14bcfcdebc7d4fc54
anaduran <ana.duran@firmwareco.com>
2021-06-11 10:52:56 -0400

    estructura basica para proyecto paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    PaisModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
